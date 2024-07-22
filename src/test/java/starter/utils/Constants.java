fd90646ce32a52a6c3f32c187d65054d2dfd7f2a
Mrarrazzaq <razaq.zeeland@gmail.com>
2024-07-22 22:56:22 +0700

    Update

--- src/test/java/starter/utils/Constants.java
+++ src/test/java/starter/utils/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
     public static String JSON_FOLDER = DIR+"/src/test/resources/JSON/";
     public static String REQ_BODY =JSON_FOLDER + "REQ_BODY/";
     public static String JSON_SCHEMA =JSON_FOLDER + "JSON_SCHEMA/";
-    public static String TOKEN_ADMIN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MSwiUm9sZSI6ImFkbWluIiwiZXhwIjoxNzIxNTA4ODg5fQ.Ss6U7enZEpv39uVMvbMcYel8Q_yPUqkFb3KtMWtYl9o";
-    public static String TOKEN_USER = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MiwiUm9sZSI6InVzZXIiLCJleHAiOjE3MjE1MDg5MDZ9.l94_wdQW3yPxCEj93gtFaSiT9kJe7nxai7xDNwguvO4";
+    public static String TOKEN_ADMIN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MSwiUm9sZSI6ImFkbWluIiwiZXhwIjoxNzIxNzQ4OTE1fQ.VGf-CbwNHhbEKi5qKElr_FcDoa6XmNLaZzMDCiDXMes";
+    public static String TOKEN_USER = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6MiwiUm9sZSI6InVzZXIiLCJleHAiOjE3MjE3NDg4MjZ9.c9qnP6NDKG6CESAFgyZ1m3phr4Y-vngw7BcLdbfJ0eU";
+    public static String IMAGE_FOLDER = DIR+"/src/test/resources/Images/";
 }
